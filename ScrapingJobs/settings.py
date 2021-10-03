c03f95244b32cd5150ddabbe069b917ca5622982
jayeshkaushik <60429482+jayeshkaushik2@users.noreply.github.com>
2021-10-03 15:06:19 +0530

    Update settings.py

--- ScrapingJobs/settings.py
+++ ScrapingJobs/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-05l1sn9r9z3uqy&(ehzxrghy-)l8s&eop$j2ypcke(edpvo%bi'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
